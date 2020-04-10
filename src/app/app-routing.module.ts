23cb40b68316bba868cbb09c95f33d9eb15672bb
sbuddha <sbuddha@ziperase.com>
2020-04-10 11:26:55 +0530

    imported component routes in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BootstrapTableComponent } from "./bootstrap-table/bootstrap-table.component";
+import { BootstrapFormComponent } from "./bootstrap-form/bootstrap-form.component";
+import { JumbotronComponent } from "./jumbotron/jumbotron.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "home"},
+  {path: "jumbotron", component: JumbotronComponent},
+  {path: "bootstrap-form", component: BootstrapFormComponent},
+  {path: "bootstrap-table", component: BootstrapTableComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
